a3c000d51e29e2a8cc51ccadd84e79631739e114
Spencer Josi <spenceraj@vt.edu>
2024-02-27 15:18:03 -0500

    test
    test

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Home";
 import Layout from "./Layout";
 
+// test
 function App() {
   return (
     <div className="App">
